f976e549dd8859555a555e4a30249a9d8c8e8ef7
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-03-09 23:47:42 +0530

    add sys call simulation time

---
+++ homework-code/measurement_1/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+
+int main(void)
+{
+        struct timeval start, end;
+
+	gettimeofday(&start, NULL);
+	read(0,NULL,0);
+	gettimeofday(&end, NULL);
+
+        long elapsed = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+	
+	printf("time: %ld\n", elapsed);
+}
